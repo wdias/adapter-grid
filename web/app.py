f37e01a867c9ba0554ae1d1403587b9ca531f14f
gihankarunarathne <gckarunarathne@gmail.com>
2019-02-16 22:28:16 +0400

    Created the docker file with support with HDF5 and NetCDF4

---
+++ web/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+from web import util
+from web.api import timeseries
+
+try:
+    assert False; import sys; sys.exit('ERROR asserts disabled, exiting')
+except AssertionError:
+    pass
+
+app = Flask(__name__)
+# Register endpoints
+app.register_blueprint(timeseries.bp)
+app.logger.info("fdsfsd")
+
+
+@app.route("/public/hc")
+def public_hc():
+    return "OK", 200
+
+
+@app.errorhandler(AssertionError)
+def handle_assertion(error):
+    ret = {'code': 400, 'error': error.args[0]}
+    app.logger.warn('ERR {code} {error}'.format(**ret),
+                    extra={'event': 'error', 'error': ret['error']})
+    print('ERR {code} {error}'.format(**ret))
+    return jsonify(**ret), ret['code']
+
+
+@app.after_request
+def log_request(response):
+    if not request.path == '/public/hc':
+        ret = {'status': response.status_code, 'request_method': request.method, 'request_uri': request.url}
+        app.logger.info("{status} {request_method} {request_uri}".format(**ret), extra=ret)
+        print("{status} {request_method} {request_uri}".format(**ret))
+    return response
