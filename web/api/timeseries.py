07dba88d0425da521824d1467190a0b91a15d86b
gihankarunarathne <gckarunarathne@gmail.com>
2019-12-14 16:33:09 +0530

    Separate creation of parallel and not parallel netCDF file into two methods

--- web/api/timeseries.py
+++ web/api/timeseries.py
@@ -5,8 +5,8 @@ import netCDF4
 import numpy as np
 from werkzeug.utils import secure_filename
 from flask import current_app as app
-from datetime import datetime, timedelta
-from web import util
+from datetime import datetime
+from web.api import util_netcdf
 
 bp = Blueprint('timeseries', __name__)
 logger = logging.getLogger(__name__)
@@ -22,62 +22,17 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def create_not_exists(filepath, timeseries_id, parallel=True):
-    try:
-        if not os.path.isfile(filepath):
-            logger.info(f'Creating new database for timeseries: {timeseries_id} ({parallel}) @ {filepath}')
-            ncfile = netCDF4.Dataset(filepath, mode='w', format=NETCDF_FILE_FORMAT, parallel=parallel)
-
-            timeseries = util.get_timeseries(timeseries_id)
-            assert 'locationId' in timeseries, f'locationId not found for Timeseries: {timeseries_id}'
-            location = util.get_regular_grid(timeseries.get('locationId'))
-
-            lat_dim = ncfile.createDimension('latitude', location.get('rows'))  # Y axis
-            lon_dim = ncfile.createDimension('longitude', location.get('columns'))  # X axis
-            time_dim = ncfile.createDimension('timestamp', None)
-
-            ncfile.moduleId = timeseries.get('moduleId')
-            ncfile.valueType = timeseries.get('valueType')
-            ncfile.parameterId = timeseries.get('parameterId')
-            ncfile.locationId = timeseries.get('locationId')
-            ncfile.timeseriesType = timeseries.get('timeseriesType')
-            ncfile.timeStepId = timeseries.get('timeStepId')
-
-            lat = ncfile.createVariable('latitude', np.float32, ('latitude',))
-            lat.units = location.get('geoDatum')
-            # logger.info("latitude: %s", lat)
-            lon = ncfile.createVariable('longitude', np.float32, ('longitude',))
-            lon.units = location.get('geoDatum')
-            time = ncfile.createVariable('timestamp', np.float64, ('timestamp',))
-            # NOTE: There's an issue with storing larger value with collective mode. In order to reduce the size, decrease the date gap
-            # time.units = "days since 1970-01-01 00:00"
-            time.units = "days since 2015-01-01 00:00"
-
-            val = ncfile.createVariable('value', np.float32, ('timestamp', 'latitude', 'longitude',))
-            val.units = timeseries.get('parameterId')
-            # Write lat and lon
-            lat[:] = location['yULCorner'] - location['yCellSize'] * np.arange(location['rows'])
-            lon[:] = location['xULCorner'] + location['xCellSize'] * np.arange(location['columns'])
-
-            ncfile.sync()
-            ncfile.close()
-        return True
-    except Exception as err:
-        logger.error(err)
-        return False
-
-
 def merge_netcdf(filename: str, timeseries_id: str):
     merge_nc = netCDF4.Dataset(os.path.join(app.config['UPLOAD_FOLDER'], filename), mode='r', format=NETCDF_FILE_FORMAT)
-    assert create_not_exists(f'/tmp/data-{timeseries_id}.nc', timeseries_id), 'Unable to create DB store'
-    ncfile = netCDF4.Dataset(f'/tmp/data-{timeseries_id}.nc', mode='r+', format=NETCDF_FILE_FORMAT, parallel=True)
-    # ncfile = netCDF4.Dataset(f'/tmp/data-{timeseries_id}.nc', mode='r+', format=NETCDF_FILE_FORMAT)
+    assert util_netcdf.create_parallel_not_exists(f'/tmp/data-{timeseries_id}.nc', timeseries_id), 'Unable to create DB store'
+    nc_file = netCDF4.Dataset(f'/tmp/data-{timeseries_id}.nc', mode='r+', format=NETCDF_FILE_FORMAT, parallel=True)
+    # nc_file = netCDF4.Dataset(f'/tmp/data-{timeseries_id}.nc', mode='r+', format=NETCDF_FILE_FORMAT)
 
     merge_time = merge_nc.variables['timestamp']
     merge_val = merge_nc.variables['value']
-    time = ncfile.variables['timestamp']
+    time = nc_file.variables['timestamp']
     time.set_collective(True)
-    val = ncfile.variables['value']
+    val = nc_file.variables['value']
     val.set_collective(True)
 
     merge_nc.set_auto_mask(False)
@@ -87,8 +42,8 @@ def merge_netcdf(filename: str, timeseries_id: str):
     time[:] = np.unique(np.append(time, times), axis=0)
 
     merge_nc.close()
-    ncfile.sync()
-    ncfile.close()
+    nc_file.sync()
+    nc_file.close()
 
 
 @bp.route("/timeseries/<string:timeseries_id>", methods=['POST'])
@@ -132,10 +87,11 @@ def timeseries_create(timeseries_id):
         return 'OK', 200
 
 
-def extract_netcdf(filename: str, timeseries_id: str, start_time: datetime, end_time: datetime):
-    nc_all = netCDF4.Dataset(f'/tmp/data-{timeseries_id}.nc', mode='r', format=NETCDF_FILE_FORMAT, parallel=True)
-    assert create_not_exists(os.path.join(app.config['UPLOAD_FOLDER'], filename), timeseries_id, parallel=False), 'Unable to extract netcdf file'
-    nc_file = netCDF4.Dataset(os.path.join(app.config['UPLOAD_FOLDER'], filename), mode='r+', format=NETCDF_FILE_FORMAT, parallel=False)
+def extract_netcdf(timeseries_id: str, request_id: str, start_time: datetime, end_time: datetime):
+    data_filename = f'/tmp/data-{timeseries_id}.nc'
+    assert os.path.isfile(data_filename), f'Timeseries {timeseries_id} data not found'
+    nc_all = netCDF4.Dataset(data_filename, mode='r', format=NETCDF_FILE_FORMAT, parallel=True)
+    nc_file = util_netcdf.get_non_parallel_netcdf_file(timeseries_id, request_id)
 
     all_time = nc_all.variables['timestamp']
     all_val = nc_all.variables['value']
@@ -167,5 +123,5 @@ def timeseries_get(timeseries_id: str, request_name):
     start_time = datetime.strptime(request.args.get('start'), DATE_TIME_FORMAT)
     assert request.args.get('end'), 'end date time should be provide'
     end_time = datetime.strptime(request.args.get('end'), DATE_TIME_FORMAT)
-    extract_netcdf(filename, timeseries_id, start_time, end_time)
+    extract_netcdf(timeseries_id, request_id, start_time, end_time)
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True, attachment_filename=request_name, mimetype='application/x-netcdf4')
---
+++ web/api/util_netcdf.py
@@ -0,0 +1,102 @@
+import logging
+import os
+import netCDF4
+import numpy as np
+from web import util
+
+logger = logging.getLogger(__name__)
+
+NETCDF_FILE_FORMAT = 'NETCDF4_CLASSIC'  # 'NETCDF4', 'NETCDF4_CLASSIC'
+ALLOWED_EXTENSIONS = set(['nc'])
+DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+TIME_FILTER = pow(10, 10)
+
+
+def create_parallel_not_exists(filepath, timeseries_id):
+    try:
+        if not os.path.isfile(filepath):
+            logger.info(f'Creating new database for timeseries: {timeseries_id} @ {filepath}')
+            nc_file = netCDF4.Dataset(filepath, mode='w', format=NETCDF_FILE_FORMAT, parallel=True)
+
+            timeseries = util.get_timeseries(timeseries_id)
+            assert 'locationId' in timeseries, f'locationId not found for Timeseries: {timeseries_id}'
+            location = util.get_regular_grid(timeseries.get('locationId'))
+            assert location and location.get('rows'), f'location date not found for Timeseries: {timeseries_id}'
+
+            lat_dim = nc_file.createDimension('latitude', location.get('rows'))  # Y axis
+            lon_dim = nc_file.createDimension('longitude', location.get('columns'))  # X axis
+            time_dim = nc_file.createDimension('timestamp', None)
+
+            nc_file.moduleId = timeseries.get('moduleId')
+            nc_file.valueType = timeseries.get('valueType')
+            nc_file.parameterId = timeseries.get('parameterId')
+            nc_file.locationId = timeseries.get('locationId')
+            nc_file.timeseriesType = timeseries.get('timeseriesType')
+            nc_file.timeStepId = timeseries.get('timeStepId')
+
+            lat = nc_file.createVariable('latitude', np.float32, ('latitude',))
+            lat.units = location.get('geoDatum')
+            # logger.info("latitude: %s", lat)
+            lon = nc_file.createVariable('longitude', np.float32, ('longitude',))
+            lon.units = location.get('geoDatum')
+            time = nc_file.createVariable('timestamp', np.float64, ('timestamp',))
+            # NOTE: There's an issue with storing larger value with collective mode. In order to reduce the size, decrease the date gap
+            # time.units = "days since 1970-01-01 00:00"
+            time.units = "days since 2015-01-01 00:00"
+
+            val = nc_file.createVariable('value', np.float32, ('timestamp', 'latitude', 'longitude',))
+            val.units = timeseries.get('parameterId')
+            # Write lat and lon
+            lat[:] = location['yULCorner'] - location['yCellSize'] * np.arange(location['rows'])
+            lon[:] = location['xULCorner'] + location['xCellSize'] * np.arange(location['columns'])
+
+            nc_file.sync()
+            nc_file.close()
+        return True
+    except Exception as err:
+        logger.error(err)
+        return False
+
+
+def get_non_parallel_netcdf_file(timeseries_id, request_id):
+    filename = f'/tmp/download-{timeseries_id}-{request_id}.nc'
+    if os.path.isfile(filename):
+        nc_file = netCDF4.Dataset(filename, mode='r+', format=NETCDF_FILE_FORMAT)
+    else:
+        logger.info('Initializing NetCDF file')
+        nc_file = netCDF4.Dataset(filename, mode='w', format=NETCDF_FILE_FORMAT)
+    # logger.info(ncfile)
+    if 'latitude' not in nc_file.dimensions or 'longitude' not in nc_file.dimensions or 'timestamp' not in nc_file.dimensions:
+        timeseries = util.get_timeseries(timeseries_id)
+        assert 'locationId' in timeseries, f'locationId not found for Timeseries: {timeseries_id}'
+        location = util.get_regular_grid(timeseries.get('locationId'))
+        assert location and location.get('rows'), f'location date not found for Timeseries: {timeseries_id}'
+
+        lat_dim = nc_file.createDimension('latitude', location.get('rows'))  # Y axis
+        lon_dim = nc_file.createDimension('longitude', location.get('columns'))  # X axis
+        time_dim = nc_file.createDimension('timestamp', None)
+        # logger.info("Dimentions: %s", ncfile.dimensions.items())
+
+        nc_file.moduleId = timeseries.get('moduleId')
+        nc_file.valueType = timeseries.get('valueType')
+        nc_file.parameterId = timeseries.get('parameterId')
+        nc_file.locationId = timeseries.get('locationId')
+        nc_file.timeseriesType = timeseries.get('timeseriesType')
+        nc_file.timeStepId = timeseries.get('timeStepId')
+
+        lat = nc_file.createVariable('latitude', np.float32, ('latitude',))
+        lat.units = "Kandawala"
+        # logger.info("latitude: %s", lat)
+        lon = nc_file.createVariable('longitude', np.float32, ('longitude',))
+        lon.units = "Kandawala"
+        time = nc_file.createVariable('timestamp', np.float64, ('timestamp',))
+        # NOTE: There's an issue with storing larger value with collective mode. In order to reduce the size, decrease the date gap
+        # time.units = "days since 1970-01-01 00:00"
+        time.units = "days since 2015-01-01 00:00"
+        val = nc_file.createVariable('value', np.float32, ('timestamp', 'latitude', 'longitude',))
+        val.units = 'O.Precipitation'
+        # Write lat and lon
+        lat[:] = location['yULCorner'] - location['yCellSize'] * np.arange(location['rows'])
+        lon[:] = location['xULCorner'] + location['xCellSize'] * np.arange(location['columns'])
+        nc_file.sync()
+    return nc_file
